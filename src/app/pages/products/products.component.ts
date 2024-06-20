c6602fbaa1c677dcf8b3f93ee81ed728361bde6d
Juan Carlos <leizar999@gmail.com>
2024-06-20 23:39:06 +0200

    Added console logs to see lifecycle changes

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'products-product-page',
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+
+  public isPublicVisible: boolean = false;
+
+  ngOnInit(): void {
+    console.log('ngOnInit')
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log({changes});
+    console.log('ngOnChanges')
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck')
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit')
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentInit')
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit')
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked')
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy')
+  }
 
 }
